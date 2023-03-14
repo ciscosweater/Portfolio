ef59406eefc9fbd21a1716bc9cd877644a4cd937
Gustavo Francisco <ciscosweater@gmail.com>
2023-03-14 19:04:52 -0300

    projetos adicionados

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ProjectCard from "../projectcard";
 import EnigmaImg from "../../assets/screenshots/enigma.jpg";
 import BatmanImg from "../../assets/screenshots/batman.png";
+import YoutubeImg from "../../assets/screenshots/youtube.png";
+import CardapioImg from "../../assets/screenshots/cardapio.png";
 import "./styles.css";
 import Footer from "../footer";
 
@@ -19,9 +21,23 @@ function Portfolio() {
                 <ProjectCard
                     image={BatmanImg}
                     link="https://ciscosweater.github.io/batman/"
-                    title="Batman"
+                    title="Batman - 2022"
                     description="Este projeto de site promocional para o filme do Batman foi desenvolvido utilizando HTML e CSS. 
                     Apresenta uma interface elegante e atraente, com um design limpo e moderno para promover o filme e atrair o público." />
+                <ProjectCard
+                    image={YoutubeImg}
+                    link="https://ciscosweater.github.io/youtube/"
+                    title="Interface do YouTube"
+                    description="Clone apenas da interface do YouTube, com responsividade para mobile,
+                    desenvolvido utilizando o React, apresenta menu lateral e header funcionais, diversas páginas,
+                    e uma aparência limpa e moderna que se aproxima do verdadeiro Youtube." />
+                <ProjectCard
+                    image={CardapioImg}
+                    link="https://ciscosweater.github.io/cardapiodelicie/"
+                    title="Cardápio Delivery"
+                    description="Cardápio interativo desenvolvido utilizando React e seus Hooks, 
+                    retorna ao usuário o seu carrinho de compras, o total a ser pago pelos itens, 
+                    e envia para o WhatsApp do estabelecimento uma mensagem com o resumo do pedido" />
             </div>
             <Footer />
         </div>
